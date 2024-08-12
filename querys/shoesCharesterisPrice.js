5d847ed0d5c0ff79fd88074f92933b46ce26b5e7
AngelPabloSC <138425945+AngelPabloSC@users.noreply.github.com>
2024-08-11 20:55:25 -0500

    sqldeb1

--- querys/shoesCharesterisPrice.js
+++ querys/shoesCharesterisPrice.js
@@ -4,20 +4,20 @@ const GET_ALL_SHOES_WITH_CHARACTERISTICS_AND_PRICES = `
   SELECT s.shoe_id, s.name AS shoe_name, s.brand_id, s.image_url, s.fk_categoryshoes,
          sc.fk_characteristics AS characteristic_id, c.name AS characteristic_name, sc.value,
          st.price AS store_price
-  FROM nv5pkm6bo5v2qwpu.shoes s
-  LEFT JOIN nv5pkm6bo5v2qwpu.shoescharacteris sc ON s.shoe_id = sc.fk_shoes
-  LEFT JOIN nv5pkm6bo5v2qwpu.characteristics c ON sc.fk_characteristics = c.characteristic_id
-  LEFT JOIN nv5pkm6bo5v2qwpu.storeShoes st ON s.shoe_id = st.fk_shoes
+  FROM zdzkxer8m5jmvjms.shoes s
+  LEFT JOIN zdzkxer8m5jmvjms.shoescharacteris sc ON s.shoe_id = sc.fk_shoes
+  LEFT JOIN zdzkxer8m5jmvjms.characteristics c ON sc.fk_characteristics = c.characteristic_id
+  LEFT JOIN zdzkxer8m5jmvjms.storeShoes st ON s.shoe_id = st.fk_shoes
 `;
 
 const GET_SHOE_WITH_CHARACTERISTICS_AND_PRICES_BY_ID = `
   SELECT s.shoe_id, s.name AS shoe_name, s.brand_id, s.image_url, s.fk_categoryshoes,
          sc.fk_characteristics AS characteristic_id, c.name AS characteristic_name, sc.value,
          st.price AS store_price
-  FROM nv5pkm6bo5v2qwpu.shoes s
-  LEFT JOIN nv5pkm6bo5v2qwpu.shoescharacteris sc ON s.shoe_id = sc.fk_shoes
-  LEFT JOIN nv5pkm6bo5v2qwpu.characteristics c ON sc.fk_characteristics = c.characteristic_id
-  LEFT JOIN nv5pkm6bo5v2qwpu.storeShoes st ON s.shoe_id = st.fk_shoes
+  FROM zdzkxer8m5jmvjms.shoes s
+  LEFT JOIN zdzkxer8m5jmvjms.shoescharacteris sc ON s.shoe_id = sc.fk_shoes
+  LEFT JOIN zdzkxer8m5jmvjms.characteristics c ON sc.fk_characteristics = c.characteristic_id
+  LEFT JOIN zdzkxer8m5jmvjms.storeShoes st ON s.shoe_id = st.fk_shoes
   WHERE s.shoe_id = ?
 `;
 
@@ -25,8 +25,8 @@ const GET_SHOE_WITH_CHARACTERISTICS_AND_PRICES_BY_ID = `
 
 const GET_SHOES_BY_BRAND = `
   SELECT s.shoe_id, s.name AS shoe_name, b.name AS brand_name
-  FROM nv5pkm6bo5v2qwpu.shoes s
-  JOIN nv5pkm6bo5v2qwpu.brands b ON s.brand_id = b.brand_id
+  FROM zdzkxer8m5jmvjms.shoes s
+  JOIN zdzkxer8m5jmvjms.brands b ON s.brand_id = b.brand_id
   WHERE s.brand_id = ?
   LIMIT 0, 1000;
 `;
@@ -34,7 +34,7 @@ const GET_SHOES_BY_BRAND = `
 
 
 module.exports = {
-  GET_ALL_SHOES_WITH_CHARACTERISTICS_AND_PRICES,
-  GET_SHOE_WITH_CHARACTERISTICS_AND_PRICES_BY_ID,
-  GET_SHOES_BY_BRAND
+    GET_ALL_SHOES_WITH_CHARACTERISTICS_AND_PRICES,
+    GET_SHOE_WITH_CHARACTERISTICS_AND_PRICES_BY_ID,
+    GET_SHOES_BY_BRAND
 };
