7101be040d3d29cb72cf00a8cbed2a7b262f00bf
AngelPabloSC <138425945+AngelPabloSC@users.noreply.github.com>
2024-08-20 08:41:00 -0500

    backid

--- models/userModels.js
+++ models/userModels.js
@@ -4,8 +4,8 @@ const db = require('../config/dbconfig.js');
 const createUser = (name, lastname, birthday_date, email, fk_category_user, callback) => {
   const query = 'INSERT INTO user (name, lastname, birthday_date, email, fk_category_user) VALUES (?, ?, ?, ?, ?)';
   db.query(query, [name, lastname, birthday_date, email, fk_category_user], (err, results) => {
-    if (err) return callback({ code: "COD_ERR", result: { error: err.message } }, null);
-    callback(null, { code: "COD_OK", id_user: results.insertId });
+    if (err) return callback(err, null);
+    callback(null, results.insertId);
   });
 };
 
@@ -13,8 +13,8 @@ const createUser = (name, lastname, birthday_date, email, fk_category_user, call
 const getAllUsers = (callback) => {
   const query = 'SELECT * FROM user';
   db.query(query, (err, results) => {
-    if (err) return callback({ code: "COD_ERR", result: { error: err.message } }, null);
-    callback(null, { code: "COD_OK", data: results });
+    if (err) return callback(err, null);
+    callback(null, results);
   });
 };
 
@@ -22,9 +22,8 @@ const getAllUsers = (callback) => {
 const getUserById = (id_user, callback) => {
   const query = 'SELECT * FROM user WHERE id_user = ?';
   db.query(query, [id_user], (err, results) => {
-    if (err) return callback({ code: "COD_ERR", result: { error: err.message } }, null);
-    if (results.length === 0) return callback({ code: "COD_ERR", result: { error: 'User not found' } }, null);
-    callback(null, { code: "COD_OK", data: results[0] });
+    if (err) return callback(err, null);
+    callback(null, results[0] || null);
   });
 };
 
@@ -32,9 +31,8 @@ const getUserById = (id_user, callback) => {
 const updateUser = (id_user, name, lastname, birthday_date, email, fk_category_user, callback) => {
   const query = 'UPDATE user SET name = ?, lastname = ?, birthday_date = ?, email = ?, fk_category_user = ? WHERE id_user = ?';
   db.query(query, [name, lastname, birthday_date, email, fk_category_user, id_user], (err, results) => {
-    if (err) return callback({ code: "COD_ERR", result: { error: err.message } }, null);
-    if (results.affectedRows === 0) return callback({ code: "COD_ERR", result: { error: 'User not found' } }, null);
-    callback(null, { code: "COD_OK", result: { message: 'User updated successfully' } });
+    if (err) return callback(err, null);
+    callback(null, results.affectedRows);
   });
 };
 
@@ -42,9 +40,8 @@ const updateUser = (id_user, name, lastname, birthday_date, email, fk_category_u
 const deleteUser = (id_user, callback) => {
   const query = 'DELETE FROM user WHERE id_user = ?';
   db.query(query, [id_user], (err, results) => {
-    if (err) return callback({ code: "COD_ERR", result: { error: err.message } }, null);
-    if (results.affectedRows === 0) return callback({ code: "COD_ERR", result: { error: 'User not found' } }, null);
-    callback(null, { code: "COD_OK", result: { message: 'User deleted successfully' } });
+    if (err) return callback(err, null);
+    callback(null, results.affectedRows);
   });
 };
 
